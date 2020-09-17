30b60b15c56ab37239e4237d7481d632a34da8d3
Ivan <DivanDoesApps@gmail.com>
2020-09-17 20:33:55 +0300

    feat: library initial configuration

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: ['prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+};
