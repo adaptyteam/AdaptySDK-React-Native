7f59aa8c48a4f0516a709a852e37ed13494e8317
Ivan <DivanDoesApps@gmail.com>
2020-09-28 22:00:57 +0300

    feat: swift event Emitters

---
+++ ios/RNAdaptyEvents.m
@@ -0,0 +1,6 @@
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
+
+@interface RCT_EXTERN_MODULE(RNAdaptyEvents, RCTEventEmitter)
+RCT_EXTERN_METHOD(check)
+@end
---
+++ ios/RNAdaptyEvents.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Adapty
+
+
+@objc(RNAdaptyEvents)
+class RNAdaptyEvents: RCTEventEmitter, AdaptyDelegate, AdaptyPaywallDelegate {
+  override static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+  
+  override func supportedEvents() -> [String]! {
+    return ["onPromoReceive", "onPurchaseSuccess", "onPurchaseFailed", "onPaywallClosed", "onInfoUpdate"]
+  }
+  
+  @objc func check() {
+    sendEvent(withName: "onPromoReceive", body: ["ma": "va"])
+  }
+  
+  func didPurchase(product: ProductModel, purchaserInfo: PurchaserInfoModel?, receipt: String?, appleValidationResult: Parameters?, paywall: PaywallViewController) {
+    sendEvent(withName: "onPurchaseSuccess", body: ["ba":"Ba"])
+  }
+  
+  func didFailPurchase(product: ProductModel, error: Error, paywall: PaywallViewController) {
+    sendEvent(withName: "onPurchaseFailed", body: ["ba":"Ba"])
+  }
+  
+  func didClose(paywall: PaywallViewController) {
+      sendEvent(withName: "onPaywallClosed", body: ["ba":"Ba"])
+  }
+  
+  func didReceiveUpdatedPurchaserInfo(_ purchaserInfo: PurchaserInfoModel) {
+      sendEvent(withName: "on", body: ["promo": purchaserInfo])
+  }
+  
+  func didReceivePromo(_ promo: PromoModel) {
+    sendEvent(withName: "onPromoReceive", body: ["promo": promo])
+  }
+  
+ 
+}
