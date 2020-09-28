8b6b7062c4ff5dac047a73ff80a8902c5f137589
Ivan <DivanDoesApps@gmail.com>
2020-09-28 21:53:44 +0300

    feat: new methods added

--- lib/src/utils/index.ts
+++ lib/src/utils/index.ts
@@ -4,6 +4,25 @@ export interface AdaptyModule {
   identify: (uId: string) => Promise<void>;
   logout: () => Promise<void>;
   updateProfile: (fields: Object) => Promise<void>;
+
+  restorePurchases: () => Promise<void>;
+  getPurchaseInfo: () => Promise<string>;
+  makePurchase: (product: string) => Promise<Object>;
+  validateReceipt: (receipt: string) => Promise<any>;
+  getPromo: () => Promise<any>;
+  getPaywalls: () => Promise<{ paywalls: string; product: string }>;
+
+  activate: (
+    sdkKey: string,
+    userId: string | any,
+    observerMode: boolean,
+    logLevel: 'errors' | 'verbose' | 'none',
+  ) => Promise<void>;
+
+  updateAttribution: (
+    object: Object,
+    source: 'Branch' | 'AppsFlyer' | 'Adjust' | 'Custom',
+  ) => Promise<void>;
 }
 
 export function extractModule(): AdaptyModule {
