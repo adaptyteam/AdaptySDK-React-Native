11301ab8de5d37f2e17ed228dffbfdf6e49fd03e
Ivan Dorofeyev <DivanDoesApps@gmail.com>
2020-10-10 20:18:54 +0300

    feat: ts metro config presented

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  projectRoot: `${__dirname}/example`,
+  watchFolders: [__dirname],
+  resolver: {
+    sourceExts: ['ts', 'tsx', 'js'],
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
