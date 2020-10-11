5c18118a49e9bf3b5c3726210c65b5556a79c808
Ivan Dorofeyev <DivanDoesApps@gmail.com>
2020-10-11 19:40:40 +0300

    optimization: [android] AdaptyPackage

--- lib/android/src/main/java/com/rnadapty/react/AdaptyPackage.java
+++ lib/android/src/main/java/com/rnadapty/react/AdaptyPackage.java
@@ -1,17 +1,14 @@
 package com.rnadapty.react;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import androidx.annotation.NonNull;
-
 import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
-
-import com.rnadapty.react.AdaptyModule;
 
 public class AdaptyPackage implements ReactPackage {
   @Override
@@ -19,9 +16,12 @@ public class AdaptyPackage implements ReactPackage {
     return Collections.emptyList();
   }
 
-  @NonNull
   @Override
-  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext  ctx) {
+  public List<NativeModule> createNativeModules(ReactApplicationContext  ctx) {
     return Arrays.<NativeModule>asList(new AdaptyModule(ctx));
+  }
+
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return null;
   }
 }
