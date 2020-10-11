605c45cccf3197710c9052127d4d7fda58fc23e5
Ivan Dorofeyev <DivanDoesApps@gmail.com>
2020-10-11 17:48:43 +0300

    feat: initial Android native example app

---
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected String getMainComponentName() {
+    return "example";
+  }
+}
