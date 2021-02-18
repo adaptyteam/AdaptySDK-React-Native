109f8b2d3f55f48261129cff1dbb20124c123c33
Ivan Dorofeyev <DivanDoesApps@gmail.com>
2021-02-18 19:49:21 +0300

    fix: lint

--- RNModels.swift
+++ RNModels.swift
@@ -29,15 +29,15 @@ extension AdaptyError: Encodable {
     case code, adaptyCode, localizedDescription
   }
   public func encode(to encoder: Encoder) throws {
-    
+
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encode(self.code, forKey: .code)
     try container.encode(self.adaptyErrorCode.rawValue, forKey: .adaptyCode)
-    
+
     if let desc = self.userInfo["NSLocalizedDescription"] as? String? {
       try container.encode(desc, forKey: .localizedDescription)
     }
-   
+
   }
-  
+
 }
