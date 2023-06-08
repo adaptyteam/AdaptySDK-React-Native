f49d0173da69df7810ae98f47f524e7e7cee30f0
divan <DivanDoesApps@gmail.com>
2023-06-08 23:19:10 +0400

    chore: Bump versions

--- lib/src/version.ts
+++ lib/src/version.ts
@@ -1,11 +1,1 @@
-let version: string | undefined;
-
-export function GET_VERSION(): string {
-  if (!version) {
-    const packageJsonPath = require.resolve('../../package.json');
-    const packageJson = require(packageJsonPath);
-    version = packageJson.version;
-  }
-
-  return version as string;
-}
+export default '2.4.7';
