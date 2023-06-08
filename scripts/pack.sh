565b70e728e2471af7e6acd66115421d63beb568
divan <DivanDoesApps@gmail.com>
2023-06-08 23:11:24 +0400

    Pack script updated

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,11 +1,17 @@
 path="examples/SubscriptionsJS/node_modules/react-native-adapty"
 
-yarn build --noEmit
+yarn build
 npm pack
 
+echo "Unzipping..."
 tar -xf react-native-adapty-*.tgz
+echo "Removing tarball..."
 rm -rf react-native-adapty-*.tgz
 
+echo "Removing previous lib..."
 rm -rf $path # remove old version
+echo "Moving to node_modules"
 mv package $path
+echo "Removing package folder..."
 rm -rf package
+  
