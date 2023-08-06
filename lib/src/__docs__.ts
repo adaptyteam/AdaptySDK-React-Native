0ccce36a3a93b995cb3aef32fc86ed33e76338d2
divan <DivanDoesApps@gmail.com>
2023-08-06 13:58:11 +0400

    Renamed docs to __docs__

--- lib/src/__docs__.ts
+++ lib/src/__docs__.ts
@@ -2,12 +2,10 @@
  * This file exposees all the API, that is needed by documentation,
  * not for the end user.
  */
-export * from './sdk/adapty';
-export * from './sdk/eventEmitter';
-export * from './sdk/error';
-
+export * from '@/adapty-handler';
+export * from '@/event-emitter';
+export * from '@/adapty-error';
 // Types
 export * from './types/error';
 export * from './types/index';
 export * from './types/inputs';
-export * from './types/events';
