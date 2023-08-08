59a1e07386fe2ee6036265cbc27565013b95807d
divan <DivanDoesApps@gmail.com>
2023-08-08 13:06:21 +0400

    Returned AdaptyError public API

--- lib/src/adapty-error.ts
+++ lib/src/adapty-error.ts
@@ -1,40 +1,71 @@
+import { ErrorCode } from '@/types/error';
+
 export interface AdaptyErrorInput {
-  adaptyCode: number;
+  adaptyCode: ErrorCode;
   message: string;
   jsError?: Error;
 }
 
 export class AdaptyError extends Error {
-  public adaptyCode: number;
+  // Custom prefix to be shown before log message
+  public static prefix = '';
+
+  // For example `2` for cancelled payment
+  public adaptyCode: ErrorCode;
+  // Message that is safe to show to a user
+  public localizedDescription: string;
+  static middleware?: (error: AdaptyError) => void;
 
   constructor(input: AdaptyErrorInput) {
-    super(input.message);
+    super(AdaptyError.getMessage(input));
+
     this.adaptyCode = input.adaptyCode;
-    this.message = input.message;
+    this.localizedDescription = input.message;
+
+    if (AdaptyError.middleware) {
+      AdaptyError.middleware(this);
+    }
   }
 
-  static failedToDecodeNativeError(
+  public static set onError(callback: (error: AdaptyError) => void) {
+    AdaptyError.middleware = callback;
+  }
+
+  public static failedToDecodeNativeError(
     message: string,
     error: unknown,
   ): AdaptyError {
     return new AdaptyError({
-      adaptyCode: -1, // TODO
+      adaptyCode: 0,
       message: message,
       jsError: error as Error,
     });
   }
 
-  static failedToEncode(message: string): AdaptyError {
+  public static failedToEncode(message: string): AdaptyError {
     return new AdaptyError({
-      adaptyCode: -1, // TODO
+      adaptyCode: 2009,
       message: message,
     });
   }
 
-  static failedToDecode(message: string): AdaptyError {
+  public static failedToDecode(message: string): AdaptyError {
     return new AdaptyError({
-      adaptyCode: -1, // TODO
+      adaptyCode: 2006,
       message: message,
     });
+  }
+
+  private static getMessage(input: AdaptyErrorInput): string {
+    const code = input.adaptyCode;
+    const codeText = ErrorCode[code];
+
+    let message = `#${code} (${codeText}): ${input.message}`;
+
+    if (AdaptyError.prefix) {
+      message = `${AdaptyError.prefix} ${message}`;
+    }
+
+    return message;
   }
 }
