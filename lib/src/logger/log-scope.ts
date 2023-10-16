98e6e70958a8863ce1474b1a3689ede0424138e1
divan <DivanDoesApps@gmail.com>
2023-10-16 14:35:30 +0400

    Added verbose logging for held native calls

--- lib/src/logger/log-scope.ts
+++ lib/src/logger/log-scope.ts
@@ -7,6 +7,8 @@ interface LogScopeConstructor extends ScopeArgs {
   onStart: LogCallback;
   onSuccess: LogCallback;
   onFailed: LogCallback;
+  onWait: LogCallback;
+  onWaitComplete: LogCallback;
 }
 
 export class LogScope {
@@ -14,16 +16,26 @@ export class LogScope {
   private onStart: LogCallback;
   private onSuccess: LogCallback;
   private onFailed: LogCallback;
+  private onWait: LogCallback;
+  private onWaitComplete: LogCallback;
 
   constructor(args: LogScopeConstructor) {
     this.methodName = args.methodName;
     this.onStart = args.onStart;
     this.onSuccess = args.onSuccess;
     this.onFailed = args.onFailed;
+    this.onWait = args.onWait;
+    this.onWaitComplete = args.onWaitComplete;
   }
 
   public start<T>(args: LogArgs<T>) {
     this.onStart(args);
+  }
+  public wait<T>(args?: LogArgs<T>) {
+    this.onWait(args);
+  }
+  public waitComplete<T>(args?: LogArgs<T>) {
+    this.onWaitComplete(args);
   }
   public success<T>(args?: LogArgs<T>) {
     this.onSuccess(args);
