0d79030d380a55a6fa6f54981357fe7a05c84c1c
divan <DivanDoesApps@gmail.com>
2023-10-17 13:29:32 +0400

    Renamed sh var from path (👺)

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,4 +1,4 @@
-path="../react-native-app/pure-0.71/node_modules/react-native-adapty"
+rn_app_dir="../react-native-app/pure-0.71/node_modules/react-native-adapty"
 
 yarn build
 npm pack # yarn pack is extremely slow 
@@ -9,9 +9,9 @@ echo "Removing tarball..."
 rm -rf react-native-adapty-*.tgz
 
 echo "Removing previous lib..."
-rm -rf $path # remove old version
+rm -rf $rn_app_dir # remove old version
 echo "Moving to node_modules"
-mv package $path
+mv package $rn_app_dir
 echo "Removing package folder..."
 rm -rf package
   
