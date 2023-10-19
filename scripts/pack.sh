a3e43b1345a67e466d4cfcd7bb525fe04f17aa56
divan <DivanDoesApps@gmail.com>
2023-10-19 13:01:38 +0400

    Temporarily added script

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,4 +1,5 @@
 rn_app_dir="../react-native-app/pure-0.71/node_modules/react-native-adapty"
+rn_ui_dir="../react-native-ui/node_modules/react-native-adapty"
 
 yarn build
 npm pack # yarn pack is extremely slow 
@@ -10,8 +11,10 @@ rm -rf react-native-adapty-*.tgz
 
 echo "Removing previous lib..."
 rm -rf $rn_app_dir # remove old version
+rm -rf $rn_ui_dir # remove old version
 echo "Moving to node_modules"
-mv package $rn_app_dir
+cp -r package $rn_app_dir
+cp -r package $rn_ui_dir
 echo "Removing package folder..."
 rm -rf package
   
