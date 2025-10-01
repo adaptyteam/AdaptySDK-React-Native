0a2c0796c35537ce6ebccac4458c68233469a753
Stanislav Mayorov <stanislavmayorav@gmail.com>
2025-10-01 16:41:04 +0300

    feat: bump RN to 0.80 in devDeps

--- jest/jest.setup.js
+++ jest/jest.setup.js
@@ -1,12 +1,22 @@
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 jest.mock('react-native', () => {
-  const RN = jest.requireActual('react-native');
-
-  RN.NativeModules.RNAdapty = {
-    getConstants: () => ({ HANDLER: 'handle' }),
-    handle: async (method, params) => {
-      return method;
+  const RN = {
+    Platform: {
+      OS: 'ios',
+      select: jest.fn((obj) => obj.ios || obj.default),
     },
+    NativeModules: {
+      RNAdapty: {
+        getConstants: () => ({ HANDLER: 'handle' }),
+        handle: async (method, params) => {
+          return method;
+        },
+      },
+    },
+    NativeEventEmitter: jest.fn(() => ({
+      addListener: jest.fn(),
+      removeListeners: jest.fn(),
+    })),
   };
 
   return RN;
