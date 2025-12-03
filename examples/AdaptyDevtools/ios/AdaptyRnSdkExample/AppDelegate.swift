67f302fe33a2160308008ed0e7230432c2fca876
Stanislav Mayorov <stanislavmayorav@gmail.com>
2025-12-03 19:53:00 +0300

    feat(example): rename AdaptyRnSdkExample to AdaptyDevtools

--- examples/AdaptyDevtools/ios/AdaptyRnSdkExample/AppDelegate.swift
+++ examples/AdaptyDevtools/ios/AdaptyRnSdkExample/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "AdaptyRnSdkExample",
+      withModuleName: "AdaptyDevtools",
       in: window,
       launchOptions: launchOptions
     )
