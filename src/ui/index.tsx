a508c0c5cd79b95ec6993277debabfbfb4934311
Stanislav Mayorov <stanislavmayorav@gmail.com>
2026-06-04 03:30:18 -0700

    chore: export AdaptyFlowViewProps from entry; refresh test AGENTS.md to Flow naming

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,5 +1,5 @@
 export { createFlowView } from './create-flow-view';
 export { createOnboardingView } from './create-onboarding-view';
 export { AdaptyOnboardingView } from './AdaptyOnboardingView';
-export { AdaptyFlowView } from './AdaptyFlowView';
+export { AdaptyFlowView, type AdaptyFlowViewProps } from './AdaptyFlowView';
 export * from './types';
